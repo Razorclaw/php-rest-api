9610ede82839c3765bc522f33020f161bc2295d2
Anthony Garcia <anthony.garcia@inria.fr>
2013-10-23 11:20:27 +0200

    chaining

--- RestApi.php
+++ RestApi.php
@@ -1,75 +1,85 @@
 <?php
 
-class ErrorHandler {
-	function raise($code, $message) {
-		header("HTTP/1.0 $code $message");
-		exit();
-	}
+class ErrorHandler
+{
+    function raise($code, $message)
+    {
+        header("HTTP/1.0 $code $message");
+        exit();
+    }
 }
 
-class RestApi {
-	private $_hs;
-	private $_format;
+class RestApi
+{
+    private $_format;
+    private $_method;
+    private $_callback;
 
-	function __construct($format = '') {
-		$this->_hs = array();
-		$this->_format = strtoupper($format);
-	}
+    function __construct($format = 'json')
+    {
+        $this->_format = strtolower($format);
+        $this->_method = strtolower($_SERVER['REQUEST_METHOD']);
+    }
 
-	function on($method, $callback) {
-		$key = strtoupper($method);
-		$this->_hs[$key] = $callback;
-	}
+    function bind($method, $callback)
+    {
+        if ($method == $this->_method)
+        {
+            $this->_callback = $callback;
+        }
+        return $this;
+    }
 
-	function handle() {
-		$rm = strtoupper($_SERVER['REQUEST_METHOD']);
-		if (array_key_exists($rm, $this->_hs)) {
-			$data = self::parse_data($rm);
-			$call = $this->_hs[$rm];
-			$e = new ErrorHandler();
-			$result = $call($data, $e);
-			echo $this->convert_result($result);
-		}
-		exit();
-	}
+    function handle()
+    {
+        $data   = $this->parse_data();
+        $result = $this->_callback($data, new ErrorHandler());
+        $this->display($result);
+        exit();
+    }
 
-	static function parse_data($rm) {
-		switch($rm) {
-			case 'POST':
-				return $_POST;
-			case 'PUT':
-				parse_str(file_get_contents("php://input"), $data);
-				return $data;
-			default:
-				return array();
-		}
-	}
+    function parse_data()
+    {
+        switch ($this->_method) {
+            case 'post':
+                return $_POST;
+            case 'put':
+                parse_str(file_get_contents("php://input"), $data);
+                return $data;
+            default:
+                return array();
+        }
+    }
 
-	function convert_result($result) {
-		switch($this->_format) {
-			case 'JSON':
-				header('Content-type: application/json');
-				return self::to_json($result);
-			case 'XML':
-				header('Content-type: text/xml');
-				return self::to_xml($result);
-			default:
-				return $result;
-		}
-	}
+    function display($result)
+    {
+        switch ($this->_format) {
+            case 'json':
+                header('Content-type: application/json');
+                echo self::to_json($result);
+                break;
+            case 'xml':
+                header('Content-type: text/xml');
+                echo self::to_xml($result);
+                break;
+            default:
+                print_r($result);
+        }
+    }
 
-	static function to_json($result) {
-		if ($json = json_encode($result)) {
-			$error = json_last_error();
+    static function to_json($result)
+    {
+        if ($json = json_encode($result)) {
+            $error = json_last_error();
 
-			// TODO: put error message
-		}
-		return $json;
-	}
+            // TODO: put error message
+        }
+        return $json;
+    }
 
-	static function to_xml($result) {
-		// TODO
-		return $result;
-	}
+    static function to_xml($result)
+    {
+        // TODO
+        return $result;
+    }
 }
-?>
--- test.php
+++ test.php
@@ -3,17 +3,14 @@ include 'RestApi.php';
 
 $api = new RestApi('json');
 
-$api->on('post', function($data) {
-	return array('method' => 'post');
-});
-
-$api->on('get', function($data, $e) {
-	$e->raise(404, "Nooot Founds");
-});
-
-$api->on('put', function($data) {
-	return $data;
-});
-
-$api->handle();
+$api->bind('post', function($data)
+{
+    return array('method' => 'post');
+})->bind('get', function($data, $e)
+{
+    $e->raise(404, "Nooot Founds");
+})->bind('put', function($data)
+{
+    return $data;
+})->handle();
 ?>
